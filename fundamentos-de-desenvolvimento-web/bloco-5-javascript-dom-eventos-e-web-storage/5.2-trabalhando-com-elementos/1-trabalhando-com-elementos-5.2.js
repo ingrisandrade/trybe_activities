099ff596f631a16fd5294dd14d66e6b856944a5a
Ingris Andrade <iac22@outlook.com>
2022-03-02 19:25:04 -0300

    Adicionando anotaçoes do arquivo JS

--- fundamentos-de-desenvolvimento-web/bloco-5-javascript-dom-eventos-e-web-storage/5.2-trabalhando-com-elementos/1-trabalhando-com-elementos-5.2.js
+++ fundamentos-de-desenvolvimento-web/bloco-5-javascript-dom-eventos-e-web-storage/5.2-trabalhando-com-elementos/1-trabalhando-com-elementos-5.2.js
@@ -3,5 +3,5 @@ console.log(document.getElementById('start').nextSibling) // nó
 console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
 
 /*
-Primeiro, foi selecionado o elemento div utilizando o seu id , 'start'. Na sequência, aplicadas as propriedades nextSibling e nextElementSibling . Você pode ver que, nextSibling retornará o texto "nó" que representa o próximo nó do DOM a partir da div , enquanto que, nextElementSibling retornará o próximo elemento propriamente, ou seja, o elemento <p> .
+No console do navegador: primeiro, foi selecionado o elemento div utilizando o seu id , 'start'. Na sequência, aplicadas as propriedades nextSibling e nextElementSibling . Você pode ver que, nextSibling retornará o texto "nó" que representa o próximo nó do DOM a partir da div , enquanto que, nextElementSibling retornará o próximo elemento propriamente, ou seja, o elemento <p> .
 */
