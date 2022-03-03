c880f2a6e67f60cfd13718b9f4869c0f123030a9
Ingris Andrade <iac22@outlook.com>
2022-03-03 10:56:27 -0300

    Adicionando anotações do bloco 7 - js es6 e testes unitários

---
+++ fundamentos-de-desenvolvimento-web/bloco-7-introducao-js-es6-e-testes-unitarios/7.1-introducao-js-es6-e-testes-unitarios/1-introducao-js-es6-e-testes-unitarios.js
@@ -0,0 +1,13 @@
+/*
+  Quando usamos o const para criar um objeto, o Javascript nos permite alterar as suas propriedades sem, contudo, reatribuir um novo objeto. Acompanhe o exemplo abaixo para entender melhor como funciona:
+  */
+
+const userInfo = {
+    name: 'Cláudio',
+    id: '5489-2',
+    email: 'claudio@email.com',
+  };
+  
+  userInfo.name = 'João';
+  
+  console.log(userInfo); // { name: 'João', id: '5489-2', email: 'claudio@email.com' }
