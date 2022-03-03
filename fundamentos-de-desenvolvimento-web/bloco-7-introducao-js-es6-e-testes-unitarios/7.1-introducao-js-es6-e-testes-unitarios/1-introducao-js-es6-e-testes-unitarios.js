2089ee81a9ccc8241fb301de6eaae19ed27698e6
Ingris Andrade <iac22@outlook.com>
2022-03-03 11:00:10 -0300

    Adicionando anotaçoes ao doc JS

--- fundamentos-de-desenvolvimento-web/bloco-7-introducao-js-es6-e-testes-unitarios/7.1-introducao-js-es6-e-testes-unitarios/1-introducao-js-es6-e-testes-unitarios.js
+++ fundamentos-de-desenvolvimento-web/bloco-7-introducao-js-es6-e-testes-unitarios/7.1-introducao-js-es6-e-testes-unitarios/1-introducao-js-es6-e-testes-unitarios.js
@@ -11,3 +11,14 @@ const userInfo = {
   userInfo.name = 'João';
   
   console.log(userInfo); // { name: 'João', id: '5489-2', email: 'claudio@email.com' }
+
+/*
+Esse raciocínio também se aplica a um array. Podemos alterá-lo indiretamente adicionando/removendo um novo item sem sobrescrever o que já foi declarado:
+*/
+
+const technologies = ['Javascript', 'CSS', 'HTML'];
+technologies.push('React');
+console.log(technologies); // [ 'Javascript', 'CSS', 'HTML', 'React' ]
+
+technologies = ['Javascript', 'CSS', 'HTML', 'React']
+console.log(technologies); // Erro
