5024d7ae7f1af11772149b70c2580d9dbaa456ad
Ingris Andrade <iac22@outlook.com>
2022-03-10 15:48:14 -0300

    Exercício de fixação feito durante aula sobre as funções: createElement() e appendChild

--- fundamentos-de-desenvolvimento-web/bloco-5-javascript-dom-eventos-e-web-storage/5.2-trabalhando-com-elementos/1-trabalhando-com-elementos-5.2.js
+++ fundamentos-de-desenvolvimento-web/bloco-5-javascript-dom-eventos-e-web-storage/5.2-trabalhando-com-elementos/1-trabalhando-com-elementos-5.2.js
@@ -1,7 +1,47 @@
-console.log(document.getElementById('start').nextSibling) // nó
+let ingredientItems = [
+ '500g de feijão carioquinha cozido',
+ '200g de toucinho',
+ '1 concha de óleo',
+ '1 cebola média picada',
+ '4 dentes de alho',
+ '5 ovos',
+ '1 colher de sopa de sal com alho',
+ 'Cheiro verde a gosto',
+ '200g de farinha de mandioca'
+];
 
-console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+const ingredientList = document.querySelector('.ingredients-list');
+
+for (let index = 0; index < ingredientItems.length; index += 1) {
+  let ingredient = ingredientItems[index];
+
+  let ingredientListItem = document.createElement('li');
+  ingredientListItem.innerText = ingredient;
+
+  ingredientList.appendChild(ingredientListItem);
+}
+
+let preparationList = [
+  'Coloque o óleo em uma panela e doure a cebola, acrescente o bacon e frite bem.',
+  'Adicione algo, sal e ovos, misture com cuidade.',
+  'Refogue o feijão, baixe o fogo, misture a farinha aos poucos e o cheiro verde.'
+];
+
+const preparationLists = document.querySelector('.recipe-preparation-list');
+
+for (let index = 0; index < preparationList.length; index += 1) {
+  let list = preparationList[index];
+
+  let listRecipe = document.createElement('li');
+  listRecipe.innerText = list;
+
+  preparationLists.appendChild(listRecipe);
+}
 
 /*
 No console do navegador: primeiro, foi selecionado o elemento div utilizando o seu id , 'start'. Na sequência, aplicadas as propriedades nextSibling e nextElementSibling . Você pode ver que, nextSibling retornará o texto "nó" que representa o próximo nó do DOM a partir da div , enquanto que, nextElementSibling retornará o próximo elemento propriamente, ou seja, o elemento <p> .
+
+console.log(document.getElementById('start').nextSibling) // nó
+
+console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
 */
