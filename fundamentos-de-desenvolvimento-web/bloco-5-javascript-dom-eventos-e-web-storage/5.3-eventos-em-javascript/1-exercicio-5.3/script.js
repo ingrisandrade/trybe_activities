f68bf6407a87f44c608a9609f3dc0d8cb089e619
Ingris Andrade <iac22@outlook.com>
2022-03-27 00:12:43 -0300

    Adiciona função que altera a cor do elemento

--- fundamentos-de-desenvolvimento-web/bloco-5-javascript-dom-eventos-e-web-storage/5.3-eventos-em-javascript/1-exercicio-5.3/script.js
+++ fundamentos-de-desenvolvimento-web/bloco-5-javascript-dom-eventos-e-web-storage/5.3-eventos-em-javascript/1-exercicio-5.3/script.js
@@ -6,7 +6,6 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'.
-
 function addNewClass(event) {
   const classElement = document.querySelector('.tech');
   classElement.classList.remove('tech');
@@ -19,7 +18,6 @@ thirdLi.addEventListener('click', addNewClass);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-
  function changeText(event) {
    const techElement = document.querySelector('.tech');
    techElement.innerText = event.target.value;
@@ -29,12 +27,51 @@ thirdLi.addEventListener('click', addNewClass);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
-
 function webPage() {
   window.location.replace('https://blog.betrybe.com/');
 }
 myWebpage.addEventListener('dblclick', webPage);
 
-/* myWebpage.addEventListener('dblclick', function() {
+/* Exemplo da Trybe:
+myWebpage.addEventListener('dblclick', function() {
   window.location.replace('https://blog.betrybe.com/');
 }) */
+
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+function changeColor(event) {
+  const evento = event.target;
+  evento.style.color = 'yellow';
+}
+myWebpage.addEventListener('mouseover', changeColor);
+
+function removeColor(event) {
+  const evento = event.target;
+  evento.style.color = 'unset';
+}
+myWebpage.addEventListener('mouseout', removeColor);
+
+/* Exemplo da Trybe:
+myWebpage.addEventListener('mouseover', function(event) {
+  event.target.style.color = 'red';
+}); 
+myWebpage.addEventListener('mouseout', function(event) {
+  event.target.style.color = 'unset';
+});*/
+
+// Segue abaixo um exemplo do uso de event.target:
+
+/* 
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText); */
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
