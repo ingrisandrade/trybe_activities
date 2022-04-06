ddc397b92b5ea39ce7b3832ccd0ffbcb93a4fb11
Ingris Andrade <iac22@outlook.com>
2022-04-06 16:38:21 -0300

    Adiciona exercícios de fixação para observação

--- fundamentos-de-desenvolvimento-web/bloco-7-introducao-js-es6-e-testes-unitarios/7.3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos-de-desenvolvimento-web/bloco-7-introducao-js-es6-e-testes-unitarios/7.3-primeiros-passos-em-jest/sum.test.js
@@ -4,7 +4,7 @@ test('sums two values', () => {
   expect(sum(2, 3)).toEqual(5);
 }); */
 
-const sum = require('./sum');
+const sum = require('./sum'); // required é utilizado para importar a função do arquivo .js
 
 test('sums two values', () => {
   expect(sum(2, 3)).toBe(5);
