53499a482b44be323dc98f6188c833b48e4c2899
Ingris Andrade <iac22@outlook.com>
2022-07-18 15:47:43 -0300

    Adding more than 1 element in App function

--- desenvolvimento-front-end/introducao_react_bloco_10/src/App.js
+++ desenvolvimento-front-end/introducao_react_bloco_10/src/App.js
@@ -1,19 +1,21 @@
-// function App() {
-//   return (
-//     <div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-function HelloWorld(nome, sobrenome) {
-  return <h1>Hello, {`${nome} ${sobrenome}`}</h1>;
+function App() {
+  return (
+    <>
+    <h1>Hello, world!</h1>
+    <h2>Teste com dois elementos HTML.</h2>
+    </>
+  );
 }
 
-const element = HelloWorld("Jorge", "Maravilha");
-const container = <div>{element}</div>;
+export default App;
 
-export default function App() {
-  return container;
-};
+// function HelloWorld(nome, sobrenome) {
+//   return <h1>Hello, {`${nome} ${sobrenome}`}</h1>;
+// }
+
+// const element = HelloWorld("Jorge", "Maravilha");
+// const container = <div>{element}</div>;
+
+// export default function App() {
+//   return container;
+// };
