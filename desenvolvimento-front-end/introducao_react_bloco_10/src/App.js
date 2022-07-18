b4a136952d6c68bfd3d301a5145cc27ee57f4e69
Ingris Andrade <iac22@outlook.com>
2022-07-18 14:03:35 -0300

    implements the course's Hello World function

--- desenvolvimento-front-end/introducao_react_bloco_10/src/App.js
+++ desenvolvimento-front-end/introducao_react_bloco_10/src/App.js
@@ -1,8 +1,19 @@
-function App() {
-  return (
-    <div>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function HelloWorld(nome, sobrenome) {
+  return <h1>Hello, {`${nome} ${sobrenome}`}</h1>;
 }
 
-export default App;
+const element = HelloWorld("Jorge", "Maravilha");
+const container = <div>{element}</div>;
+
+export default function App() {
+  return container;
+};
