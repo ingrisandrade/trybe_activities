8c4df7845081da46476a92c69fc8872ddfdbefea
Ingris Andrade <iac22@outlook.com>
2022-07-18 22:33:02 -0300

    create appointment list

--- desenvolvimento-front-end/exercises_react_bloco_10/exercises/10.1/src/App.js
+++ desenvolvimento-front-end/exercises_react_bloco_10/exercises/10.1/src/App.js
@@ -1,2 +1,23 @@
+import React, { Component } from "react";
 
+// const Task = (value) => {
+//   return (
+//     <li>{value}</li>
+//   );
+// }
 
+class Task extends Component {
+  render() {
+    const commitments = ['Study React', 'Perform exercises', 'Participate in monitoring', 'Complete 80% on project day'];
+    return (
+      <div>
+        {
+          commitments.map((task) => <h1 key={task}>Commitment { task }</h1>)
+        }
+
+      </div>
+    );
+  }
+}
+
+export default Task;
