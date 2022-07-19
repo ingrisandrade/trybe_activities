413216bc1ce51cd37be83517cb90acf708d7f827
Ingris Andrade <iac22@outlook.com>
2022-07-19 12:37:13 -0300

    refactor element li

--- desenvolvimento-front-end/exercises_react_bloco_10/exercises/10.1/src/App.js
+++ desenvolvimento-front-end/exercises_react_bloco_10/exercises/10.1/src/App.js
@@ -11,9 +11,12 @@ class Task extends Component {
     const commitments = ['Study React', 'Perform exercises', 'Participate in monitoring', 'Complete 80% on project day'];
     return (
       <div>
-        {
-          commitments.map((task) => <h1 key={task}>Commitment { task }</h1>)
-        }
+
+        { commitments.map((task) => <h1 key={task}>Commitment { task }</h1>) }
+
+        <ul>
+          { commitments.map((task) => <li> { task } </li> ) };
+        </ul>
 
       </div>
     );
