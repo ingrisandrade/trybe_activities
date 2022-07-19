d2b49d42ab3b90da899a9b9858556a15325541b4
Ingris Andrade <iac22@outlook.com>
2022-07-18 21:16:27 -0300

    create card through class

--- desenvolvimento-front-end/introducao_react_bloco_10/src/Card.js
+++ desenvolvimento-front-end/introducao_react_bloco_10/src/Card.js
@@ -1,11 +1,21 @@
+import React, { Component } from "react";
 
+class Card extends Component {
+  render () {
+    return(
+      <h1>Fundamentals</h1>
+    );
+  };
+};
+
+export default Card;
 
 // My first REACT component.
 
-function Card() {
-  return(
-    <h1>Fundamentals</h1>
-  )
-}
+// function Card() {
+//   return(
+//     <h1>Fundamentals</h1>
+//   )
+// }
 
-export default Card;
+// export default Card;
