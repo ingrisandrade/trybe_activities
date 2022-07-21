3e266e035427edc15c6e04b8e4dbc92e3bd07152
Ingris Andrade <iac22@outlook.com>
2022-07-21 12:33:34 -0300

    Adds 3 buttons with different texts

--- desenvolvimento-front-end/exercises_react_bloco_11/fixation-exercises-11-1/src/App.jsx
+++ desenvolvimento-front-end/exercises_react_bloco_11/fixation-exercises-11-1/src/App.jsx
@@ -1,23 +1,52 @@
 import React from "react";
 
+// Crie uma aplicação React com npx create-react-app fancy-buttons . Altere o componente App.js para que seja um componente de classe e contenha um botão associado a um evento que imprime um texto qualquer via console.log(). Não precisa se preocupar com a sintaxe correta para funções de eventos por enquanto!
+
+// Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log().
+
+function buttonClick() {
+  console.log('clicked on the button');
+}
+
+function buttonClickTwo() {
+  console.log('clicked on the button 2');
+}
+
+function buttonClickThree() {
+  console.log('clicked on the button 3');
+}
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.buttonClick = this.buttonClick.bind(this);
-
-    console.log('Component being built');
-  }
-
-  buttonClick() {
-    console.log(this);
-    console.log('clicked on the button');
-  }
   render() {
-    console.log(this);
-    return <button onClick={ this.buttonClick }>Click Here</button>
+    return(
+    <>
+    <button onClick={ buttonClick }>Click Here</button>
+    <button onClick={ buttonClickTwo }>Click Here</button>
+    <button onClick={ buttonClickThree }>Click Here</button>
+    </>
+    );
   }
 }
 
 export default App;
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+
+//     this.buttonClick = this.buttonClick.bind(this);
+
+//     console.log('Component being built');
+//   }
+
+//   buttonClick() {
+//     console.log(this);
+//     console.log('clicked on the button');
+//   }
+//   render() {
+//     console.log(this);
+//     return <button onClick={ this.buttonClick }>Click Here</button>
+//   }
+// }
+
+// export default App;
