2a872b559f39c514623848af6a5bb8a0bcda186a
Ingris Andrade <iac22@outlook.com>
2022-07-21 12:35:10 -0300

    delete comments with function

--- desenvolvimento-front-end/exercises_react_bloco_11/fixation-exercises-11-1/src/App.jsx
+++ desenvolvimento-front-end/exercises_react_bloco_11/fixation-exercises-11-1/src/App.jsx
@@ -29,24 +29,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// class App extends React.Component {
-//   constructor() {
-//     super()
-
-//     this.buttonClick = this.buttonClick.bind(this);
-
-//     console.log('Component being built');
-//   }
-
-//   buttonClick() {
-//     console.log(this);
-//     console.log('clicked on the button');
-//   }
-//   render() {
-//     console.log(this);
-//     return <button onClick={ this.buttonClick }>Click Here</button>
-//   }
-// }
-
-// export default App;
