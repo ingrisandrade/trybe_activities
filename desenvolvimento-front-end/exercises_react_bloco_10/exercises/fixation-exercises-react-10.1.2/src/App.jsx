d35a43ed76154a3cb597730ebd466910aa1b01db
Ingris Andrade <iac22@outlook.com>
2022-07-26 11:36:54 -0300

    add component footer

--- desenvolvimento-front-end/exercises_react_bloco_10/exercises/fixation-exercises-react-10.1.2/src/App.jsx
+++ desenvolvimento-front-end/exercises_react_bloco_10/exercises/fixation-exercises-react-10.1.2/src/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import Header from "./Header";
 import Content from "./Content";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <Header />
         <Content />
+        <Footer />
       </div>
     );
   }
