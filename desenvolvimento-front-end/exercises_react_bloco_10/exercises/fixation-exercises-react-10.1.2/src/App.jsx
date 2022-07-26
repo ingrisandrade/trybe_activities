12f5162968c8acb3ae362d2699fef52e7665b97c
Ingris Andrade <iac22@outlook.com>
2022-07-26 11:36:19 -0300

    add h1 element

--- desenvolvimento-front-end/exercises_react_bloco_10/exercises/fixation-exercises-react-10.1.2/src/App.jsx
+++ desenvolvimento-front-end/exercises_react_bloco_10/exercises/fixation-exercises-react-10.1.2/src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { Component } from "react";
 import Header from "./Header";
 import Content from "./Content";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return(
       <div>
