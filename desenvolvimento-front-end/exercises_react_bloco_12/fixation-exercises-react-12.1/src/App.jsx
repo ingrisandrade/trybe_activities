a75dfae1e951d1a07f1faec56b247b694db2fc28
Ingris Andrade <iac22@outlook.com>
2022-08-05 12:35:43 -0300

    importa comp Counter

--- desenvolvimento-front-end/exercises_react_bloco_12/fixation-exercises-react-12.1/src/App.jsx
+++ desenvolvimento-front-end/exercises_react_bloco_12/fixation-exercises-react-12.1/src/App.jsx
@@ -1,8 +1,14 @@
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import React, { Component } from "react";
+import Counter from "./Counter";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Counter />
+      </div>
+    )
+  }
 }
 
 export default App;
